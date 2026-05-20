d2a60a63a50184f5705cf6b95551b062d68bc446
krisha-3 <kri385385@gmail.com>
2026-05-20 22:15:26 +0530

    Innstall Monaco Editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { Provider } from "./components/ui/provider";
+import CodeEditor from "./components/CodeEditor";
 
 function App() {
   return (
     <Provider>
-      <Box minH='100vh' bg='#3D3030' color={"gray.500"} px={6} py={8}>WebStudio</Box>
+      <Box minH="100vh" bg="#3D3030" color={"gray.500"} px={6} py={8}>
+        <CodeEditor />
+      </Box>
     </Provider>
   );
 }
--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Provider } from "./ui/provider";
 import { Box } from "@chakra-ui/react";
+import Editor from "@monaco-editor/react";
+// import { editor } from "monaco-editor";
 
 function CodeEditor() {
+  const editorRef = useRef();
+  const [value, setValue] = useState("");
+  const onMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
   return (
     <Provider>
       <Box>
-        
+        <Editor
+          height="75vh"
+          theme="vs-dark"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          onMount={onMount}
+          value={value}
+          onChange={(value) => setValue(value)}
+        />
+        ;
       </Box>
     </Provider>
   );
